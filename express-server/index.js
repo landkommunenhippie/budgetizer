f60508770851703521ccebb246fe19305d98cd78
Tobias Stubenrauch <t.stubenrauch@senec.com>
2021-12-09 22:52:18 +0100

    added simple express server to save json files based on URL-Params

---
+++ express-server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 3000
+
+const BUDGETIZER_JSON_DIR = './data'
+
+app.use(express.json());
+
+app.put('/:user/:type', (req, res) => {
+	console.log(JSON.stringify(req.body));
+	fs.writeFile(`${BUDGETIZER_JSON_DIR}/${req.params.user}-${req.params.type}.json`, JSON.stringify(req.body), function(err) {
+    if(err) {
+        console.log(err);
+				throw new Error(err);
+    }
+    res.send("OK");
+}); 
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
